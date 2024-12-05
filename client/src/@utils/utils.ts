9864e6e7aa559438d69d795f390b009bfda590e9
Abhishek Pattanayak <2021.abhishek.pattanayak@ves.ac.in>
2024-12-06 01:22:43 +0530

    add: priority color function

--- client/src/@utils/utils.ts
+++ client/src/@utils/utils.ts
@@ -9,3 +9,7 @@ export function toggleTheme () : void {
   localStorage.setItem('theme', localStorage.getItem('theme') === 'dark' ? 'light' : 'dark');
   document.documentElement.classList.toggle('dark');
 }
+
+export function priorityColor (priority: string) : string {
+  return priority === "High" ? "bg-red-400" : priority === "Medium" ? "bg-amber-400" : "bg-green-400"
+}
